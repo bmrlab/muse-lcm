97eb14aea280eb0d8e1da88c36d28d94741bb353
zhuojg <zhuojg1519@outlook.com>
2023-11-29 19:41:34 +0800

    fix: update pipeline

--- app/lcm.py
+++ app/lcm.py
@@ -41,22 +41,24 @@ def handle_request(image_base64: str, prompt: str):
 
     pil_img = load_image(get_image_from_base64(image_base64))
 
-    if cached_prompt_embedding is None or prompt != cached_prompt:
-        # tokenize the prompt
-        prompt_inputs = pipeline.tokenizer(
-            prompt, return_tensors="pt", padding="max_length"
-        ).to("cuda")
-        # create prompt encoding
-        prompt_embeds = pipeline.text_encoder(**prompt_inputs)
-        # extract CLIP embedding
-        prompt_embeds = prompt_embeds["last_hidden_state"]
+    # if cached_prompt_embedding is None or prompt != cached_prompt:
+    #     # tokenize the prompt
+    #     prompt_inputs = pipeline.tokenizer(
+    #         prompt, return_tensors="pt", padding="max_length"
+    #     ).to("cuda")
+    #     # create prompt encoding
+    #     prompt_embeds = pipeline.text_encoder(**prompt_inputs)
+    #     # extract CLIP embedding
+    #     prompt_embeds = prompt_embeds["last_hidden_state"]
 
-        cached_prompt_embedding = prompt_embeds
-        cached_prompt = prompt
+    #     cached_prompt_embedding = prompt_embeds
+    #     cached_prompt = prompt
 
-    image = pipeline(
-        image=pil_img, prompt_embeds=cached_prompt_embedding, **default_params
-    ).images[0]
+    # image = pipeline(
+    #     image=pil_img, prompt_embeds=cached_prompt_embedding, **default_params
+    # ).images[0]
+
+    image = pipeline(prompt=prompt, image=pil_img, **default_params).images[0]
 
     base64_string = get_base64_from_image(image)
 
@@ -109,5 +111,7 @@ async def update_pipeline(req: PipelineRequest):
     try:
         pipeline_module = importlib.import_module(f"app.pipelines.{req.pipeline}")
         pipeline, default_params = pipeline_module.build_pipeline()
+
+        return {"status": "ok"}
     except Exception as e:
         print(f"failed to update pipeline{req.pipeline}: {e}")
